8198af8b19a6d2d1fb9941f80f10a3363dd24a92
Danohmite <Danomite84@gmail.com>
2020-11-24 09:54:36 -0800

    post pull from master

--- hack-team-1/src/pages/Home.js
+++ hack-team-1/src/pages/Home.js
@@ -2,76 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-
-    <div className="v36_360">
-      <div className="v36_361">What</div>
-      <div className="v36_362">Would</div>
-      <div className="v36_363">Elate</div>
-      <div className="v36_364">You?</div>
-      <div className="name"></div>
-      <div className="v36_367"></div>
-      <div className="v36_368"></div>
-      <div className="v36_369"></div>
-      <div className="v36_370"></div>
-      <div className="v36_371"></div>
-      <div className="v36_372"></div>
-      <div className="v36_373"></div>
-      <div className="v36_374"></div>
-      <div className="v36_375"></div>
-      <div className="v36_376"></div>
-      <div className="v36_377"></div>
-      <div className="v36_378"></div>
-      <div className="v36_379"></div>
-      <div className="v36_380"></div>
-      <div className="v36_381"></div>
-      <div className="v36_382"></div>
-      <div className="v36_383"></div>
-      <div className="v36_384"></div>
-      <div className="v36_385"></div>
-      <div className="v36_386"></div>
-      <div className="v36_387"></div>
-      <div className="v36_388"></div>
-      <div className="v36_389"></div>
-      <div className="v36_390"></div>
-      <div className="v36_391"></div>
-      <div className="v36_392"></div>
-      <div className="v36_393"></div>
-      <div className="v36_394"></div>
-      <div className="v36_395"></div>
-      <div className="v36_396"></div>
-      <div className="v36_397"></div>
-      <div className="v36_398"></div>
-      <div className="v36_399"></div>
-      <div className="v36_400"></div>
-      <div className="v36_401"></div>
-      <div className="v36_402"></div>
-      <div className="v36_403"></div>
-      <div className="v36_404"></div>
-      <div className="v36_405"></div>
-      <div className="v36_406"></div>
-      <div className="v36_407"></div>
-      <div className="v36_408"></div>
-      <div className="v36_409"></div>
-      <div className="v36_410"></div>
-      <div className="v36_411"></div>
-      <div className="v36_412"></div>
-      <div className="v36_413"></div>
-      <div className="v36_414"></div>
-      <div className="v36_415"></div>
-      <div className="v36_416"></div>
-      <div className="v36_417"></div>
-      <div className="v36_418"></div>
-      <div className="v36_419"></div>
-      <div className="v36_420"></div>
-      <div className="v36_421"></div>
-      <div className="name"></div>
-      <div className="v36_428">Go</div>
-=======
     <div>
       <div className="killMee"></div>
       <div className="laugh" ></div>
->>>>>>> a92ed0d84e36e36de332e1de3048a81a32880182
     </div>
 
   );
