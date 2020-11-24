a60ff637b1f0dd69738f44eaa8ad35a3eef8274e
Danohmite <Danomite84@gmail.com>
2020-11-23 21:16:55 -0800

    navbar1

--- hack-team-1/src/App.js
+++ hack-team-1/src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
+import React from 'react';
 import Api from './components/api-one';
 import Jokes from './components/AwfulJokes';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import './App.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    loggedIn: false
+  }
+  loggedIn = () => {
+    this.setState({ loggedIn: !this.state.loggedIn })
+  }
+
+  render() {
   return (
     <div className="App">
+      <Navbar loggedIn={ this.loggedIn } signedIn={ this.state.loggedIn }/>
       <h1>HELLO WORLD</h1>
-      <Api />
+      {/* <Api /> */}
       <Jokes />
     </div>
   );
+  }
 }
 
 export default App;
