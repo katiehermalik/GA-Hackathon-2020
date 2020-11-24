79d7c87e1adb4beee01fbe82bf8cbf4053bbf4e1
khermalik <khermalik@gmail.com>
2020-11-24 11:59:17 -0800

    home buttons

--- hack-team-1/src/App.js
+++ hack-team-1/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Api from './components/api-one';
-import Jokes from './components/AwfulJokes';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import './App.css';
+import routes from './config/routes';
 
 class App extends React.Component {
   state = {
@@ -17,9 +16,8 @@ class App extends React.Component {
   return (
     <div className="App">
       <Navbar loggedIn={ this.loggedIn } signedIn={ this.state.loggedIn }/>
-      <Home />
-      {/* <Api /> */}
-      {/* <Jokes /> */}
+      { routes }
+
     </div>
   );
   }
